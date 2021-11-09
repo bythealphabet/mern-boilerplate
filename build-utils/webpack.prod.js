1eff005bc552069982f58429d5c827e736320740
bythealphabet <bythealphabet@gmail.com>
2021-11-09 16:25:16 -0400

    adding changes to package.js prod update

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,20 +1,19 @@
 const path = require("path");
-const Visualizer = require("webpack-visualizer-plugin");
 
 const CURRENT_WORKING_DIR = process.cwd();
 
-module.exports = {
+const config = {
   mode: "production",
-  entry: [path.join(CURRENT_WORKING_DIR, "/client/main.js")],
+  entry: [path.join(CURRENT_WORKING_DIR, "client/main.js")],
   output: {
-    path: path.join(CURRENT_WORKING_DIR, "/dist/web/"),
+    path: path.join(CURRENT_WORKING_DIR, "/dist"),
     filename: "bundle.js",
-    publicPath: "/dist/web/",
+    publicPath: "/dist/",
   },
   module: {
     rules: [
       {
-        test: /\.(ts|js)x?$/i,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
       },
@@ -22,11 +21,22 @@ module.exports = {
         test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
         use: "file-loader",
       },
+      {
+        test: /\.md$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+          {
+            loader: "markdown-loader",
+            options: {
+              /* your options here */
+            },
+          },
+        ],
+      },
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js"],
-  },
-
-  plugins: [new Visualizer({ filename: "./statistics.html" })],
 };
+
+module.exports = config;
